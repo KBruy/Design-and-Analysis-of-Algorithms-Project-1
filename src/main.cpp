d8ce20006f4a1459ce348fc7f7ca76f76191b0e0
KBury <krystian.bury1@gmail.com>
2026-04-04 15:41:50 +0200

    feat: implementacja tablicy dynamicznej

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include "../include/Movie.h"
+#include "../include/DynamicArray.h"
 
 int main(){
-    Movie movie("Inception", 8.8);
+    DynamicArray movies;
 
-    std::cout << "Tytul: "<< movie.getTitle() << std::endl;
-    std::cout << "Ranking: " << movie.getRanking() << std::endl;
+    movies.pushBack(Movie("Inception", 8.8));
+    movies.pushBack(Movie("b", 10.0));
+    movies.pushBack(Movie("c", 2.9));
+
+    for (int i = 0; i < movies.getSize(); i++){
+        std::cout << "Tytul: "<< movies.get(i).getTitle() << std::endl;
+        std::cout << "Ranking:" << movies.get(i).getRanking() << std::endl;
+        std::cout << std::endl;
+    }
 
     return 0;
 }
