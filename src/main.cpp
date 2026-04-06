439b88b21ecf9e373a462b2b70662c1d81dd01f5
KBury <krystian.bury1@gmail.com>
2026-04-06 14:35:13 +0200

    feat: przygotowywanie zbiorów danych o określonych rozmiarach na pomiary

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,16 @@ int main() {
 
     DynamicArray data10000 = filter.prepareDataSet(filteredMovies, 10000);
     DynamicArray data100000 = filter.prepareDataSet(filteredMovies, 100000);
+    DynamicArray data500000 = filter.prepareDataSet(filteredMovies, 500000);
+    DynamicArray data1000000 = filter.prepareDataSet(filteredMovies, 1000000);
+    DynamicArray dataMax = filter.prepareDataSet(filteredMovies, filteredMovies.getSize());
 
     std::cout << "Liczba filmow po filtrowaniu: " << filteredMovies.getSize() << std::endl;
     std::cout << "Rozmiar zbioru 10000: " << data10000.getSize() << std::endl;
     std::cout << "Rozmiar zbioru 100000: " << data100000.getSize() << std::endl;
+    std::cout << "Rozmiar zbioru 500000: " << data500000.getSize() << std::endl;
+    std::cout << "Rozmiar zbioru 1000000: " << data1000000.getSize() << std::endl;
+    std::cout << "Rozmiar zbioru max: " << dataMax.getSize() << std::endl;
 
     return 0;
 }
